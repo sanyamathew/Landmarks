23a1d5e730abe1612eae86ed6960e9cac514a2ba
Sanya Mathew <sanya.a.mathew@gmail.com>
2022-10-07 13:46:49 -0700

    .....

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,13 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            MapView()
+                .ignoresSafeArea(edges:.top)
+                .frame(height:300)
+            
+            CircleImage()
+                .offset(y:-130)
+                .padding(.bottom, -130)
+            
+            VStack (alignment: .leading){
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                Text("About Turtle Rock")
+                    .font(.subheadline)
+                Text("Turtle rock is a popular hang out for turtles looking to sunbathe and get tan!")
+            }
+            .padding()
+            Spacer()
         }
-        .padding()
+        
     }
 }
 
